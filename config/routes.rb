36c8c9bf3c854a79fc05c07e571ba0d045522e4c
jakeNorman007 <jacob.a.norman91@gmail.com>
2024-08-19 23:51:00 -0400

    setting up devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  # devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :v1 do
     resources :guests
     resources :rooms
     resources :bookings
+    resources :sessions, only: [:create, :destroy]
   end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
