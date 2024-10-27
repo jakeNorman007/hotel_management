1cb0911fb53dfdc941aa090462d61c5ae54109ca
jakeNorman007 <jacob.a.norman91@gmail.com>
2024-10-27 00:06:49 -0400

    few minor cosmetic things

--- client/src/components/MainNav.vue
+++ client/src/components/MainNav.vue
@@ -18,10 +18,11 @@ const logOutUser = () => {
 
 <template>
   <div class="flex flex-col border border-r-black gap-6">
-    <div class="font-semibold text-3xl flex justify-center mt-[3rem]">
-      <h1>Hotel Management</h1>
+    <div class="font-semibold text-3xl flex flex-col justify-center mt-[3rem] ml-[4rem]">
+      <p>Swift Stay.</p>
+      <p>Pro.</p>
     </div>
-    <div class="flex flex-col gap-2 mt-[3rem]">
+    <div class="flex flex-col gap-2 mt-[1rem]">
       <router-link :to="{ name: 'Dashboard' }" class="flex justify-start items-center pl-5 py-3 font-semibold text-gray-600 hover:bg-green-200 hover:text-green-600 gap-2 mx-2">
         <DashboardIcon/>
         <p>Dashboard</p>
